21fc51d2503a75b987853e25e532ef73e171f29c
Quentin Lintz <quentinlintz@outlook.com>
2024-10-29 21:58:06 -0400

    Update lab10-riscv Makefile, format assembly

--- lab10-riscv/big2little.s
+++ lab10-riscv/big2little.s
@@ -3,34 +3,35 @@
 .global _start
 
 .section .text
+
 _start:
-  # Set up
-  li t0, 0x300
-  li t1, 0x12
-  jal ra, store
-  li t1, 0x34
-  jal ra, store
-  li t1, 0xAB
-  jal ra, store
-  li t1, 0xCD
-  jal ra, store
-  li t0, 0x300
-  
-  # Start swapping
-  lb t2, 0(t0)
-  sb t1, 0(t0)
-  sb t2, 3(t0)
-  lb t2, 1(t0)
-  lb t3, 2(t0)
-  sb t2, 2(t0)
-  sb t3, 1(t0)
-  
-  j end
-  
+# Set up
+li  t0, 0x300
+li  t1, 0x12
+jal ra, store
+li  t1, 0x34
+jal ra, store
+li  t1, 0xAB
+jal ra, store
+li  t1, 0xCD
+jal ra, store
+li  t0, 0x300
+
+# Start swapping
+lb t2, 0(t0)
+sb t1, 0(t0)
+sb t2, 3(t0)
+lb t2, 1(t0)
+lb t3, 2(t0)
+sb t2, 2(t0)
+sb t3, 1(t0)
+
+j end
+
 store:
-  sb t1, 0(t0)
-  addi t0, t0, 1
-  jr ra
+	sb   t1, 0(t0)
+	addi t0, t0, 1
+	jr   ra
 
 end:
-  addi a0, zero, 0
+	addi a0, zero, 0
--- lab10-riscv/div9.s
+++ lab10-riscv/div9.s
@@ -5,20 +5,25 @@
 .global _start
 
 .section .text
+
 _start:
-  addi a0, zero, 9 # Start with the input
-  beq a0, zero, div # If the input is 0, it is divisible by 9
+	addi a0, zero, 9 # Start with the input
+	beq  a0, zero, div # If the input is 0, it is divisible by 9
+
 dec:
-  addi a0, a0, -9 # Subtract 9 from the input
-  beq a0, zero, div # If the input is 0, it is divisible by 9
-  blt a0, zero, not # If the input is negative, it is not divisible by 9
-  j not 
+	addi a0, a0, -9 # Subtract 9 from the input
+	beq  a0, zero, div # If the input is 0, it is divisible by 9
+	blt  a0, zero, not # If the input is negative, it is not divisible by 9
+	j    not
+
 div:
-  addi a0, zero, 1
-  j done
+	addi a0, zero, 1
+	j    done
+
 not:
-  addi a0, zero, 0
-  j done
+	addi a0, zero, 0
+	j    done
+
 done:
-  addi a7, zero, 93
-  ecall
+	addi a7, zero, 93
+	ecall
